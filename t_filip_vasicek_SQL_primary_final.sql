e52ef1a57b27d0f35569807301b9921f62a8929a
vasicekf <filda.vasicek@gmail.com>
2024-10-04 02:08:36 +0200

    Add files via upload

---
+++ t_filip_vasicek_SQL_primary_final.sql
@@ -0,0 +1,33 @@
+CREATE TABLE t_filip_vasicek_SQL_primary_final AS
+SELECT 
+    p.payroll_year AS year,
+    p.payroll_quarter AS quarter,
+    pib.name AS industry_branch,
+    p.value AS wage,
+    pc.name AS food_category,
+    cp.value AS food_price,
+    pc.price_value AS food_price_per_unit,
+    pc.price_unit AS unit,
+    r.name AS region_name,
+    d.name AS district_name
+FROM 
+    czechia_payroll p
+JOIN 
+    czechia_payroll_industry_branch pib ON p.industry_branch_code = pib.code
+JOIN 
+    czechia_price cp ON p.payroll_year = YEAR(cp.date_from)
+JOIN 
+    czechia_price_category pc ON cp.category_code = pc.code
+JOIN 
+    czechia_payroll_value_type pvt ON p.value_type_code = 5958
+JOIN 
+    czechia_payroll_calculation calc ON p.calculation_code = 100
+JOIN 
+    czechia_region r ON cp.region_code = r.code
+JOIN 
+    czechia_district d ON r.code = LEFT(d.code, 5)
+WHERE 
+    p.payroll_year IS NOT NULL 
+    AND cp.date_from IS NOT NULL 
+    AND p.value IS NOT NULL
+    AND p.payroll_year BETWEEN 2010 AND 2012;
